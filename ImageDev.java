a3461eee04401eaec91f2ace2d8661aff15e9fe6
MiguelPV1 <72744184+MiguelPV1@users.noreply.github.com>
2020-10-12 07:51:47 -0500

    Added comments on code
    Added comments on code: java code

--- ImageDev.java
+++ ImageDev.java
@@ -1,4 +1,10 @@
 /*
+  Author: MIGUEL PV
+  Creation Date: 1/10/2020
+  Last Updated: 12/10/2020
+*/
+
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
